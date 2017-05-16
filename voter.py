bc3cb02c42afe1bc62f5f134d0d3e06381fa36d0
André Ilhicas dos Santos <andre.ilhicas@fiercely.pt>
2017-05-16 23:45:40 +0100

    ADD async pool to clients

--- voter.py
+++ voter.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 from zeep import Client
+from multiprocessing.dummy import Pool
+import collections
+import random
 
 app = Flask(__name__)
 client_0 = Client('http://localhost:9000/soap/insulincalculator?wsdl')
@@ -11,9 +14,65 @@ class Voter():
     #TODO Class implementation
     def __init__(self):
         self.todo = 'TODO'
+        self.pool = Pool(3)
+        self.clients = clients
+        self.results = []
+
+    def collect_results(self, result):
+        self.results.append(result)
+
+    def _mealtimeInsulinDose(self, carbo_meal, carbo_proc, act_blood_sugar, tgt_blood_sugar, sensivity):
+        clients_ids = random.sample(xrange(1,4), 3)
+        for _id in clients_ids:
+            self.pool.apply_async(self.clients[_id].mealtimeInsulinDose, (carbo_meal, carbo_proc, act_blood_sugar, tgt_blood_sugar, sensivity,), callback= self.collect_results)
+            if len(self.results) > 1 and self.vote():
+                self.pool.terminate()
+                return self.vote(discard=True)
+        
+        self.vote(discard=True)
+        return None
     
-    def _call_client(self):
-        pass
+    def _backgroundInsulinDose(self, weight):
+        clients_ids = random.sample(xrange(1,4), 3)
+        for _id in clients_ids:
+            self.pool.apply_async(self.clients[_id].mealtimeInsulinDose, (weight,), callback= self.collect_results)
+            if len(self.results) > 1 and self.vote():
+                self.pool.terminate()
+                return self.vote(discard=True)
+        
+        self.vote(discard=True)
+        return None
+    
+    def _personalSensitivityToInsulin(self, activity_level, k_activity, k_drops):
+        clients_ids = random.sample(xrange(1,4), 3)
+        for _id in clients_ids:
+            self.pool.apply_async(self.clients[_id].mealtimeInsulinDose, (activity_level, k_activity, k_drops,), callback= self.collect_results)
+            if len(self.results) > 1 and self.vote():
+                self.pool.terminate()
+                return self.vote(discard=True)
+        
+        self.vote(discard=True)
+
+        return None
+
+    def vote(self, discard=False):
+        voter = collections.Counter(self.results)
+        
+        if 3 in voter.keys():
+            if discard:
+                self.results = []
+            return voter[3]
+
+        if 2 in voter.keys():
+            if discard:
+                self.results = []
+            return voter[2]
+        
+        if discard:
+            self.results = []
+        #No majority reached , return None
+        return None
+            
 
 #Global Voter
 voter = Voter()
