05b9c5bae11a46f2a36430dbb4fc4784a93857ed
André Ilhicas dos Santos <andre.ilhicas@fiercely.pt>
2017-05-21 23:24:36 +0100

    FIX voter to allow retry, refactor personalsensitivitytoinsulin

--- voter.py
+++ voter.py
@@ -6,87 +6,198 @@ import random
 import time
 
 app = Flask(__name__)
-client_0 = Client('http://10.17.1.23:8081/insulin?wsdl')
-client_1 = Client('http://10.17.1.3:8080/services/InsulinDoseCalculator?wsdl')
-client_2 = Client('http://10.17.1.8:8080/InsulinDoseCalculator/ws/insulin-dose-calculator?wsdl')
-clients = [client_0, client_1, client_2]
 
-class Voter():
-    #TODO Class implementation
+class ClientProxy():
+    def __init__(self, url):
+        self.url = url
+        self.proxy = Client(url)
+
+    def backgroundInsulinDose(self, weight): 
+        started = time.time()
+        return [int(self.proxy.service.backgroundInsulinDose(weight)), self.url, (time.time() - started)]
+    
+    def mealtimeInsulinDose(self, carbo_meal, carbo_proc, act_blood_sugar, tgt_blood_sugar, sensivity):
+        started = time.time()
+        return [int(self.proxy.service.mealtimeInsulinDose(carbo_meal, carbo_proc, act_blood_sugar, tgt_blood_sugar, sensivity)), self.url, (time.time() - started)]
+    
+    def personalSensitivityToInsulin(self, activity_level, k_activity, k_drops):
+        started = time.time()
+        return [int(self.proxy.service.personalSensitivityToInsulin(activity_level, k_activity, k_drops)), self.url, (time.time() - started)]
+
+class ClientList():
+    
     def __init__(self):
-        self.todo = 'TODO'
+        self.clients = []
+        self.setup()
+    
+    def get_clients(self):
+        return self.clients
+
+    def setup(self):
+        try:
+            client = ClientProxy("http://10.17.1.23:8081/insulin?wsdl")
+            self.clients.append(client)
+        except:
+            pass
+        try:
+            client = ClientProxy("http://10.17.1.8:8080/InsulinDoseCalculator/ws/insulin-dose-calculator?wsdl")
+            self.clients.append(client)
+        except:
+            pass
+        try:    
+            client = ClientProxy("http://10.17.1.21:8081/insulincalculator?wsdl")
+            self.clients.append(client)
+        except:
+            pass
+        try:
+            client = ClientProxy("http://10.17.1.17:8081/insulindosecalculator?wsdl")
+            self.clients.append(client)
+        except:
+            pass
+        try:
+            client = ClientProxy("http://10.17.1.13:8080/InsulinDoseCalculator_WebService_QCS/services/InsulinDoseCalculator?wsdl")
+            self.clients.append(client)
+        except:
+            pass
+        try:
+            client = ClientProxy("http://10.17.1.16:8080/deploy/services/insulin?wsdl")
+            self.clients.append(client)
+        except:
+            pass
+        try:
+            client = ClientProxy("http://10.17.1.25:9000/soap/insulincalculator?wsdl")
+            self.clients.append(client)
+        except:
+            pass
+        try:
+            client = ClientProxy("http://10.17.1.10:8080/WebServiceQCS/services/InsulinDoseCalculator?wsdl")
+            self.clients.append(client)
+        except:
+            pass
+        try:
+            client = ClientProxy("http://10.17.1.26:9999/InsulinCalculator?wsdl")
+            self.clients.append(client)
+        except:
+            pass
+        try:
+            client = ClientProxy("http://10.17.1.6:8080/InsulinDoseCalculator/insulin?wsdl")
+            self.clients.append(client)
+        except:
+            pass
+        try:    
+            client = ClientProxy("http://10.17.1.24:8081/InsulinDoseCalculatorEndpoint?wsdl")
+            self.clients.append(client)
+        except:
+            pass
+        try:
+            client = ClientProxy("http://10.17.1.14:9000/InsulinDoseCalculator?wsdl")
+            self.clients.append(client)
+        except:
+            pass
+        try:
+            client = ClientProxy("http://10.17.1.19:8080/insulin/insulin?wsdl")
+            self.clients.append(client)
+        except:
+            pass
+        try:
+            client = ClientProxy("http://ec2-54-218-110-193.us-west-2.compute.amazonaws.com:8081/mf_calculator?wsdl")
+            self.clients.append(client)
+        except:
+            pass
+    
+class Voter():
+    
+    def __init__(self, client_list):
         self.pool = Pool(3)
-        self.clients = clients
-        self.results = []
+        self.clients = client_list
+        self.results = list()
         self.time_to_live = 4.00
+        self.show_results = dict()
+
     def collect_results(self, result):
-        self.results.append(int(result))
-        print self.results
+        
+        self.results.append(result[0])
+        self.show_results[result[1]] = [result[0], result[2]]
+        
 
-    def _mealtimeInsulinDose(self, carbo_meal, carbo_proc, act_blood_sugar, tgt_blood_sugar, sensivity):
-        started = time.time()
-        clients_ids = random.sample(xrange(0,3), 3)
+    def _mealtimeInsulinDose(self, carbo_meal, carbo_proc, act_blood_sugar, tgt_blood_sugar, sensivity, started = None):
+        
+        clients_ids = random.sample(xrange(0,len(self.clients)), 3)
+        if started is None:
+            started = time.time()
+
         for _id in clients_ids:
-            self.pool.apply_async(self.clients[_id].service.mealtimeInsulinDose, (carbo_meal, carbo_proc, act_blood_sugar, tgt_blood_sugar, sensivity,), callback= self.collect_results)
+            self.pool.apply_async(self.clients[_id].mealtimeInsulinDose, (carbo_meal, carbo_proc, act_blood_sugar, tgt_blood_sugar, sensivity,), callback= self.collect_results)
             if len(self.results) > 1 and self.vote():
                 self.pool.terminate()
-                return self.vote(discard=True)
-
+                return self.vote(), self.show_results
+        
         while((time.time() - started < self.time_to_live) and not self.vote()):
-            pass
-
+            if len(self.results) == 3:
+                self.results = []
+                self._mealtimeInsulinDose(carbo_meal, carbo_proc, act_blood_sugar, tgt_blood_sugar, sensivity, started=started)
+        
         self.pool.terminate()
-        return self.vote(discard=True)
+        return self.vote(), self.show_results
 
 
-    def _backgroundInsulinDose(self, weight):
-        started = time.time()
-        clients_ids = random.sample(xrange(0,3), 3)
+    def _backgroundInsulinDose(self, weight, started = None):
+        clients_ids = random.sample(xrange(0,len(self.clients)), 3)
+        if started is None:
+            started = time.time()
+
         for _id in clients_ids:
-            self.pool.apply_async(self.clients[_id].service.backgroundInsulinDose, (weight,), callback= self.collect_results)
+            self.pool.apply_async(self.clients[_id].backgroundInsulinDose, (weight,), callback= self.collect_results)
             if len(self.results) > 1 and self.vote():
                 self.pool.terminate()
-                return self.vote(discard=True)
-
+                return self.vote(), self.show_results
+        
         while((time.time() - started < self.time_to_live) and not self.vote()):
-            pass
-
+            if len(self.results) == 3:
+                self.results = []
+                self._backgroundInsulinDose(weight, started=started)
+            
+        
         self.pool.terminate()
-        return self.vote(discard=True)
+        return self.vote(), self.show_results
 
-    def _personalSensitivityToInsulin(self, activity_level, k_activity, k_drops):
-        started = time.time()
-        clients_ids = random.sample(xrange(0,3), 3)
+    def _personalSensitivityToInsulin(self, activity_level, k_activity, k_drops, started=None):
+        clients_ids = random.sample(xrange(0,len(self.clients)), 3)
+        if started is None:
+            started = time.time()
+
         for _id in clients_ids:
-            self.pool.apply_async(self.clients[_id].service.mealtimeInsulinDose, (activity_level, k_activity, k_drops,), callback= self.collect_results)
+            self.pool.apply_async(self.clients[_id].personalSensitivityToInsulin, (activity_level, k_activity, k_drops,), callback= self.collect_results)
             if len(self.results) > 1 and self.vote():
                 self.pool.terminate()
-                return self.vote(discard=True)
+                return self.vote(), self.show_results
 
         while((time.time() - started < self.time_to_live) and not self.vote()):
-            pass
+            if len(self.results) == 3:
+                self.results = []
+                self._personalSensitivityToInsulin(activity_level, k_activity, k_drops, started=started)
 
         self.pool.terminate()
-        return self.vote(discard=True)
+        return self.vote(), self.show_results
 
-    def vote(self, discard=False):
+    def vote(self):
         voter = collections.Counter(self.results)
-
         try:
             voter = dict(voter)
             for result, votes in voter.iteritems():
-                if votes == 3 or votes == 2:
+                if votes >= 2:
                     return result
 
             #No majority reached , return None
         except Exception as e:
             #Object counter is not yet built
-            print e
+            pass
+            
         return None
 
 
-#Global Voter
-voter = Voter()
+#Global Clients List
+client_list = ClientList().get_clients()
 
 @app.route("/", methods=["GET"])
 def index():
@@ -116,7 +227,8 @@ def mealtimeInsulinDose():
 
     # Call webservices and do voting stuff for mealtimeInsulinDose here
     # ....
-    response = Voter()._mealtimeInsulinDose(carbo_meal, carbo_proc, act_blood_sugar, tgt_blood_sugar, sensitivity)
+    
+    response = Voter(client_list)._mealtimeInsulinDose(carbo_meal, carbo_proc, act_blood_sugar, tgt_blood_sugar, sensitivity)
 
     response = str(response)
     return response
@@ -136,10 +248,9 @@ def backgroundInsulinDose():
 
     # Call webservices and do voting stuff for backgroundInsulinDose here
     # ....
-    response = Voter()._backgroundInsulinDose(weight)
-
-    response = str(response)
-    return response
+    response = Voter(client_list)._backgroundInsulinDose(weight)
+    
+    return str(response)
 
 
 """Determines an individual's sensitivity to one unit of insulin
@@ -151,20 +262,35 @@ Returns: Background insulin dose
 """
 @app.route("/personalSensitivityToInsulin", methods=["GET"])
 def personalSensitivityToInsulin():
+    carbo_meal = request.args.get('carbo_meal')
+    carbo_proc = request.args.get('carbo_proc')
+    act_blood_sugar = request.args.get('act_blood_sugar')
+    tgt_blood_sugar = request.args.get('tgt_blood_sugar')
     activity_level = request.args.get('activity_level')
     k_activity = request.args.getlist('k_activity')
     k_drops = request.args.getlist('k_drops')
     if not activity_level or not k_activity or not k_drops:
         return render_template("index.html", response="Failed to calculate")
+    k_activity_final = list()
+    k_drops_final = list()
+    for k in k_activity:
+        if k:
+            k_activity_final.append(int(k))
+    for k in k_drops:
+        if k:
+            k_drops_final.append(int(k))
 
-
+    print k_activity_final
+    print k_drops_final
     # Call webservices and do voting stuff for personalSensitivityToInsulin here
     # ....
-    response = Voter()._personalSensitivityToInsulin(activity_level, k_activity, k_drops)
+    activity_level = int(activity_level)
+
+    response, first_details = Voter()._personalSensitivityToInsulin(activity_level, k_activity_final, k_drops_final)
+    response, second_details = Voter()._mealtimeInsulinDose(carbo_meal, carbo_proc, act_blood_sugar, tgt_blood_sugar, response)
 
     response = str(response)
     return response
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, threaded=True)
